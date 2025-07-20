f060f3f01106ccbafac68ac073c0098774475639
André Sena <andresena1506@gmail.com>
2025-07-20 15:17:29 -0300

    Update writeCSV.ts

--- Semana 3/model/writeCSV.ts
+++ Semana 3/model/writeCSV.ts
@@ -2,9 +2,9 @@ import { createObjectCsvWriter as createCsvWriter } from 'csv-writer';
 import { Data } from "C:/Users/andre/OneDrive/Desktop/iJunior/Semana 2/model/interfaceData";
 
 
-const writeCSV = async ("C:/Users/andre/OneDrive/Desktop/iJunior/Semana 2/service/serviceEstoque.ts": string, data: Data[]): Promise<void> => {
+const writeCSV = async ("C:/Users/andre/OneDrive/Desktop/iJunior/Semana 2/model/estoque.csv": string, data: Data[]): Promise<void> => {
   const csvWriter = createCsvWriter({
-    path: "C:/Users/andre/OneDrive/Desktop/iJunior/Semana 2/service/serviceEstoque.ts",
+    path: "C:/Users/andre/OneDrive/Desktop/iJunior/Semana 2/model/estoque.csv",
     header: [
       { id: 'nome', title: 'NOME' },
       { id: 'valor', title: 'VALOR' },
