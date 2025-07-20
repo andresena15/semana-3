6db5a5b8ad442d0ee0e2c6f90132a26513184b78
André Sena <andresena1506@gmail.com>
2025-07-20 15:19:25 -0300

    Update readCSV.ts

--- Semana 3/model/readCSV.ts
+++ Semana 3/model/readCSV.ts
@@ -1,11 +1,11 @@
 import fs from 'fs';
 import csv from 'csv-parser';
-import { Data } from "C:/Users/andre/OneDrive/Desktop/iJunior/Semana 2/model/interfaceData";
+import { Data } from ".../model/interfaceData";
 
-const readCSV = async ("C:/Users/andre/OneDrive/Desktop/iJunior/Semana 2/model/estoque.csv": string): Promise<Data[]> => {
+const readCSV = async (".../model/estoque.csv": string): Promise<Data[]> => {
   return new Promise((resolve, reject) => {
     const results: Data[] = [];
-    fs.createReadStream("C:/Users/andre/OneDrive/Desktop/iJunior/Semana 2/model/estoque.csv")
+    fs.createReadStream(".../model/estoque.csv")
       .pipe(csv())
       .on('data', (data: Data) => results.push(data))
       .on('end', () => resolve(results))
