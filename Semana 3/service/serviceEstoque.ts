09117c85cd21a82efce6bc3c7180117bfc775beb
André Sena <andresena1506@gmail.com>
2025-07-20 21:27:31 -0300

    Update serviceEstoque.ts

--- Semana 3/service/serviceEstoque.ts
+++ Semana 3/service/serviceEstoque.ts
@@ -15,4 +15,79 @@ class estoqueService{
             await writeCSV(filePath, [data] );
         }
     }
+    async remover(produto: string){
+        const todosOsProdutos = await readCSV(filePath);
+        const produtoEncontrado = todosOsProdutos.find(produto => produto.nome.toLowerCase() === nome.toLowerCase());
+        if(produtoEncontrado != undefined){
+            produtoEncontrado = null;
+            console.log("Produto removido.");
+        }
+        else{
+            throw new Error('Não foi encontrado esse Produto');
+        }
+    }
+    async listar(){
+        const todosOsProdutos = await readCSV(filePath);
+        for(var a = 0; todosOsProdutos[a] !=null; a++){
+            console.log("Nome: " +todosOsProdutos[a].nome+ "Valor: " +todosOsProdutos[a].valor+ "Peso:" + todosOsProdutos[a].peso+ "Quantidade: " +todosOsProdutos[a].quantidade+);
+        }
+    }
+
+    async valorTotal(){
+        const todosOsProdutos = await readCSV(filePath);
+        var preçoTotal: number = 0;
+        for(var a = 0; todosOsProdutos[a] !=null; a++){
+            preçoTotal = preçoTotal + todosOsProdutos[a].valor;
+        }
+        return preçoTotal;
+    }
+
+    async pesoTotal(){
+        const todosOsProdutos = await readCSV(filePath);
+        var kilosTotal: number = 0;
+        for(var a = 0; todosOsProdutos[a] !=null; a++){
+            kilosTotal = kilosTotal + todosOsProdutos[a].peso;
+        }
+        return kilosTotal;
+    }
+
+    async valorMédio(){
+        const todosOsProdutos = await readCSV(filePath);
+        var preçoTotal: number = 0;
+        var quantidadeTotal:number = 0;
+        for(var a = 0; todosOsProdutos[a] !=null; a++){
+            preçoTotal = preçoTotal + todosOsProdutos[a].valor;
+            quantidadeTotal = quantidadeTotal +todosOsProdutos[a].quantidade;
+        }
+        return preçoTotal/quantidadeTotal;
+    }
+
+    async pesoMédio(){
+        const todosOsProdutos = await readCSV(filePath);
+        var kilosTotal: number = 0;
+        var quantidadeTotal:number = 0;
+        for(var a = 0; todosOsProdutos[a] !=null; a++){
+            kilosTotal = kilosTotal + todosOsProdutos[a].peso;
+            quantidadeTotal = quantidadeTotal + todosOsProdutos[a].quantidade;
+        }
+        return kilosTotal/quantidadeTotal;
+    }
+
+    async quantidadeTotal(){
+        const todosOsProdutos = await readCSV(filePath);
+        var quantidadeTotal:number = 0;
+        for(var a = 0; todosOsProdutos[a] != null; a++){
+            quantidadeTotal = quantidadeTotal + todosOsProdutos[a].quantidade;
+        }
+        return quantidadeTotal;
+    }
+
+    
+
+    
+
+    
+
+    
+    
 }
